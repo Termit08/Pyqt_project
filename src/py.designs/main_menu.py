81385ebb3e4fe351d94a0d03cc5b8614366d54fe
EgorAtamanenko <158510901+Termit08@users.noreply.github.com>
2024-12-08 16:45:26 +0300

    Update main_menu.py

--- src/py.designs/main_menu.py
+++ src/py.designs/main_menu.py
@@ -1,40 +1,39 @@
-from PyQt6 import QtCore, QtWidgets
-
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 class Ui_MainMenu(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(764, 756)
-        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label_2 = QtWidgets.QLabel(parent=self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(130, 160, 521, 71))
-        self.label_2.setStyleSheet("font: 36pt \"MS Shell Dlg 2\";\n"
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.pixmap = QtGui.QPixmap("./data/phones/main_menu.jpg")
+        if self.pixmap.isNull():
+                print("Изображение не загружено!")
+        self.label.resize(764, 756)
+        self.label.move(0, 0)
+        self.label.setPixmap(self.pixmap)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(180, 160, 521, 71))
+        self.label_2.setStyleSheet("font: 36pt \"Arial\";\n"
 "color: rgb(255, 255, 255);")
         self.label_2.setObjectName("label_2")
-        self.starting = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.starting = QtWidgets.QPushButton(self.centralwidget)
         self.starting.setGeometry(QtCore.QRect(100, 300, 531, 101))
         self.starting.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-"\n"
-"font: 26pt \"MS Shell Dlg 2\";\n"
-"border-color: rgb(0, 0, 0);\n"
-"")
+        "\n"
+        "font: 26pt \"Arial\";\n"
+        "border-color: rgb(0, 0, 0);\n"
+        "")
         self.starting.setObjectName("starting")
-        self.instructions = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.instructions = QtWidgets.QPushButton(self.centralwidget)
         self.instructions.setGeometry(QtCore.QRect(100, 430, 531, 101))
         self.instructions.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-"font: 26pt \"MS Shell Dlg 2\";\n"
-"border-color: rgb(0, 0, 0);")
+        "font: 26pt \"Arial\";\n"
+        "border-color: rgb(0, 0, 0);")
         self.instructions.setObjectName("instructions")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 764, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
@@ -44,5 +43,3 @@ class Ui_MainMenu(object):
         self.label_2.setText(_translate("MainWindow", "ГЛАВНОЕ МЕНЮ"))
         self.starting.setText(_translate("MainWindow", "Начать игру"))
         self.instructions.setText(_translate("MainWindow", "Инструкция"))
-
-
