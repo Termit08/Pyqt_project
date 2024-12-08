2192deeb85404759e43ee54c58271e3f8ec9363e
EgorAtamanenko <158510901+Termit08@users.noreply.github.com>
2024-12-08 17:03:07 +0300

    Update window_with_instruction.py

--- src/window_with_instruction.py
+++ src/window_with_instruction.py
@@ -1,7 +1,5 @@
-import sys
-
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt6.QtWidgets import QLabel, QMainWindow
 
 SCREEN_SIZE = [600, 700]
 
@@ -10,17 +8,8 @@ class Instruction(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(0, 0, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение картинки')
-
-        self.pixmap = QPixmap('photo_instruction.jpg')
+        self.pixmap = QPixmap('./data/designs_picture/instruction.jpg')
         self.image = QLabel(self)
         self.image.move(60, 0)
         self.image.resize(800, 800)
         self.image.setPixmap(self.pixmap)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Instruction()
-    ex.show()
-    sys.exit(app.exec())
